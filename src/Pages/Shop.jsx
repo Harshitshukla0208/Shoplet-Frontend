681b1df64c373fdb01b900485b9d1e773af09e0a
Harshit Shukla <mrharshit2409@gmail.com>
2024-03-02 18:45:47 +0530

    Added multiple components on home page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from '../Components/Home/Banner'
+import Popular from '../Components/Popular/Popular'
+import Offers from '../Components/Offers/Offers'
 
 const Shop = () => {
     return (
         <div>
             <Home />
+            <Popular />
+            <Offers />
         </div>
     )
 }
