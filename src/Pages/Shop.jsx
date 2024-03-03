27e3f05a23baa69bdadeb8aa852947ef6b57351a
Harshit Shukla <mrharshit2409@gmail.com>
2024-03-03 17:02:22 +0530

    Added few more components on shop page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Home from '../Components/Home/Banner'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollections from '../Components/NewCollections/NewCollections'
+import NewsLetter from '../Components/NewsLetter/NewsLetter'
 
 const Shop = () => {
     return (
@@ -9,6 +11,8 @@ const Shop = () => {
             <Home />
             <Popular />
             <Offers />
+            <NewCollections />
+            <NewsLetter />
         </div>
     )
 }
