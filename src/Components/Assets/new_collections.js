c3a596bc6ced9bdd3e78adf1d35f6b33d834f5ca
Harshit Shukla <mrharshit2409@gmail.com>
2024-03-03 20:20:56 +0530

    ui fix

--- src/Components/Assets/new_collections.js
+++ src/Components/Assets/new_collections.js
@@ -8,13 +8,13 @@ import p7_img from "./product_17.png";
 import p8_img from "./product_28.png";
 
 let new_collections = [
-  {
-    id: 12,
-    name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
-    image: p1_img,
-    new_price: 50.0,
-    old_price: 80.5,
-  },
+  // {
+  //   id: 12,
+  //   name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+  //   image: p1_img,
+  //   new_price: 50.0,
+  //   old_price: 80.5,
+  // },
   {
     id: 35,
     name: "Boys Orange Colourblocked Hooded Sweatshirt",
@@ -36,13 +36,13 @@ let new_collections = [
     new_price: 100.0,
     old_price: 150.0,
   },
-  {
-    id: 15,
-    name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
-    image: p5_img,
-    new_price: 50.0,
-    old_price: 80.5,
-  },
+  // {
+  //   id: 15,
+  //   name: "Men Green Solid Zippered Full-Zip Slim Fit Bomber Jacket",
+  //   image: p5_img,
+  //   new_price: 50.0,
+  //   old_price: 80.5,
+  // },
   {
     id: 2,
     name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
