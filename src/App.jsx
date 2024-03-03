7403922b3150eb6069f21401885b7279e42d8752
Harshit Shukla <mrharshit2409@gmail.com>
2024-03-03 19:21:09 +0530

    Added products acc. to categories

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Product from './Pages/Product'
 import Cart from './Pages/Cart'
 import LoginSignup from './Pages/LoginSignup'
 import Footer from './Components/Footer/Footer'
+import mens_banner from './Components/Assets/banner_mens.png'
+import womens_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 function App() {
   return (
@@ -18,9 +21,9 @@ function App() {
         <Routes>
           
           <Route path = '/' element = {<Shop />} />
-          <Route path = '/mens' element = {<ShopCategory category = "men" />} />
-          <Route path = '/womens' element = {<ShopCategory category = "women" />} />
-          <Route path = '/kids' element = {<ShopCategory category = "kid" />} />
+          <Route path = '/mens' element = {<ShopCategory banner={mens_banner} category = "men" />} />
+          <Route path = '/womens' element = {<ShopCategory banner={womens_banner} category = "women" />} />
+          <Route path = '/kids' element = {<ShopCategory banner={kids_banner} category = "kid" />} />
 
           <Route path = '/product' element = {<Product/>}>
             <Route path=':productId' element = {<Product />} />
