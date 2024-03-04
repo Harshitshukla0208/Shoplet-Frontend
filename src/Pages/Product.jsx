1dee04b47af3063a7cd775935991b130954808f6
Harshit Shukla <mrharshit2409@gmail.com>
2024-03-04 17:32:12 +0530

    Added individual product page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import {ShopContext} from '../Context/ShopContext'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+    
+    const {all_product} = useContext(ShopContext); //fetching all products
+    const {productId} = useParams(); //getting a particular product id
+    const product = all_product.find((e) => e.id === Number(productId)); //storing that particular product and 'Number()' used to convert string to int coz product id is in string
+
     return (
         <div>
-        
+            <Breadcrum product={product} />
+            <ProductDisplay product={product} /> { /*using this prop we will display the details of that product */}
         </div>
     )
 }
