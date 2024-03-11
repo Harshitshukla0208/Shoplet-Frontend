aef148fd8f84e2be03bbb8fa0b94b4a0d644ac1a
Harshit Shukla <mrharshit2409@gmail.com>
2024-03-12 00:47:37 +0530

    Made login and signup function and linked it with backend api

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -21,9 +21,43 @@ const LoginSignup = () => {
 
     const login = async() => {
         console.log("login function executed", formData)
+        let responseData;
+        await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/form-data',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }).then((response) => response.json()).then((data) => responseData = data)
+
+        if(responseData.success){ //if this is true that means token has been generated
+            localStorage.setItem('auth-token', responseData.token);
+            window.location.replace("/"); //after a successful login it will redirect to the home page
+        }
+        else{
+            alert(responseData.errors)
+        }
     }
     const signup = async() => {
         console.log("signup function executed", formData)
+        let responseData;
+        await fetch('http://localhost:3000/signup', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/form-data',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }).then((response) => response.json()).then((data) => responseData = data)
+
+        if(responseData.success){ //if this is true that means token has been generated
+            localStorage.setItem('auth-token', responseData.token);
+            window.location.replace("/"); //after a successful login it will redirect to the home page
+        }
+        else{
+            alert(responseData.errors)
+        }
     }
 
 
